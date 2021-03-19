ec90f283fe9df984ae7988ce85320a7d708ef918
Lee2SH000 <melisa000627@gmail.com>
2021-03-19 11:24:46 +0900

    Add Madlib Python project_commit

---
+++ madlib.py
@@ -0,0 +1,21 @@
+# string concatenation
+#food = ""
+
+#print("I want to have"+food+" for my dinner.")
+#print("I want to have {} for my dinner.".format(food))
+#print(f"I want to have {food} for my dinner.")
+
+
+adj1 = input("Adjective: ")
+verb1 = input("Verb: ")
+adj2 = input("Adjective: ")
+verb2 = input("Verb: ")
+adj3 = input("Adjective: ")
+verb3 = input("Verb: ")
+person = input("Person name: ")
+verb4 = input("Verb: ")
+madlib = (f"Today is a {adj1} day. As I am planning on {verb1} cupcake, which is my {adj2}. \
+I love to {verb2}, because it makes me feel {adj3}. Stay passionate and {verb3} like you are {person}.\
+Then you will be able to {verb4}.")
+
+print(madlib)
